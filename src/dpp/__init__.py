9e2d7bcdb92e990551a66f60f08e6611f05a067c
Abhijeet-Real <abhijeet1472@gmail.com>
2025-03-22 03:12:18 +0530

    Update import statement in DataAnalysis notebook and increment package version to 0.1.2

--- src/dpp/__init__.py
+++ src/dpp/__init__.py
@@ -1,15 +1,2 @@
-# src/datapreprocessor/__init__.py
-
-"""
-datapreprocessor - A modular data preprocessing package.
-
-Example usage:
-import datapreprocessor as dpp
-obj = dpp(dataset, "target")
-"""
-
 from .DPP import DataPreprocessor
-
 __all__ = ["DataPreprocessor"]
-import sys
-sys.modules["datapreprocessor"] = DataPreprocessor
