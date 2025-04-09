7b89c9f414e608623559ddd7022b98365c0dae5a
Abhijeet-Real <abhijeet1472@gmail.com>
2025-04-09 20:46:55 +0530

    Refactor regression evaluation and plotting utilities
    - Updated the `evaluate_regression` function to remove unnecessary flattening of predictions and to compute RMSE for each output separately, enhancing clarity in the output.
    - Modified the `plot.py` utility to streamline the plotting functions for regression and classification outputs by directly referencing the respective functions, improving code readability and maintainability.

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -1,8 +1,5 @@
 from . import plot_regression_outputs as pro
 from . import plot_classification_outputs as pco
 
-def plot_model_outputs(history):
-        pro.plot_regression_outputs(history)
-
-def plot_model_outputs(y_test, y_pred):
-        pco.plot_classification_outputs(y_test, y_pred)
+plot_regression_outputs = pro.plot_regression_outputs
+plot_classification_outputs = pco.plot_classification_outputs
